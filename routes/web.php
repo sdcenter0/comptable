c47f849fe618330d60dce2a3181c56a1c10e079c
EL BAHJA Mohamed <elbahjamohamed0@gmail.com>
2021-04-08 15:34:29 +0100

    Installig laravel/voyager package

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
